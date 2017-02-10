55feefb1b78fbac6abeb2be940cc2913db9a74aa
Sean Kelker <seankelker@gmail.com>
2017-02-09 21:23:09 -0800

    Network Tables Working :)

---
+++ build/CMakeFiles/cv_vision.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_vision.dir/vision.cpp.o"
+  "cv_vision.pdb"
+  "cv_vision"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
